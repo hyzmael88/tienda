68297e78e97c7b5867eb75b61ea99aa4aa2f022e
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2022-11-30 00:19:28 -0600

    tienda nextjs

---
+++ components/HeroBanner.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Link from 'next/link'
+import {urlFor} from '../lib/client'
+
+function HeroBanner({heroBanner}) {
+  return (
+    <div className='hero-banner-container'>
+      <div>
+        <h3 className='beats-solo'>
+    	      {heroBanner.smallText}
+        </h3>
+        <h3>
+          {heroBanner.midText}
+        </h3>
+        <h1>
+          {heroBanner.largeText1}
+        </h1>
+        <img src={urlFor(heroBanner.image)} alt='headphones' className='hero-banner-image' />
+      </div>
+
+      <div>
+        <Link href={`/product/${heroBanner.product}`}>
+          <button type='button'>{heroBanner.buttonText}</button>
+        </Link>
+        <div className='desc'>
+          <h5>Description</h5>
+          <p>{heroBanner.desc}</p>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default HeroBanner
